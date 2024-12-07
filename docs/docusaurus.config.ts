b50165b33572bfa95818f117d659f83d2fef3425
Kyle Machulis <kyle@nonpolynomial.com>
2024-12-07 15:28:25 -0800

    doc: Create simple site social card

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo-social-card.png',
     navbar: {
       title: 'QRTuber',
       logo: {
